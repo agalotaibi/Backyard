aa7b7ef231e24f7befb900ba2ebcc948b1ae371d
Amaal Al Otaibi <hopes.gmo@gmail.com>
2022-10-29 16:24:14 +0300

    Initial Commit

---
+++ Backyard/BackyardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackyardApp.swift
+//  Backyard
+//
+//  Created by Amaal on 04/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct BackyardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
